0e51fb0b06d3101ee29f7e142dff6c84ec4f5da6
Maks Tish <decim.meri@gmail.com>
2026-04-21 21:40:24 +0300

    change app, add view.js and main.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
-
 class App {
+    //паблик свойство
+    route = [               
+        {path: "", view: null}
+    ];
 
+    constructor() {
+        window.addEventListener('hashchange', this.route.bind(this));   //биндим на тек. контекст
+        this.route();
+    }
+    //Функция роутинга
+    route() {               
+        const view = this.route.find(r => r.path == location.hash).view;
+    }
 }
 
 new App();
