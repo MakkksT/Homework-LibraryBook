af0743cb652eb65b900106cd24c9aa6fdcf8208c
Maks Tish <decim.meri@gmail.com>
2026-04-28 03:49:42 +0300

    add render-loader card-list.js

--- src/components/card-list/card-list.js
+++ src/components/card-list/card-list.js
@@ -1,35 +1,21 @@
 import { DivComponent } from "../../common/div-components";
-import './search.css';
+import './card-list.css';
 export class CardList extends DivComponent {
-    constructor(appState, parentState) { //обновлять избранное, пэррент для проверки загрузки
+    constructor(appState, parentState) { //обновлять избранное, пэррент для проверки загрузки кол-во книг
         super();
         this.appState = appState;
         this.parentState = parentState;
     }
 
-
     //Метод рендер переопределяем
     render() {
-        if (this.parentState.loading)
-        this.el.classList.add('card-list');
+        if (this.parentState.loading) {
+            this.el.innerHTML = `<div class="card_list__loader">Загрузка...</div>`;
+            return this.el;
+        }
         this.el.innerHTML = `
-            <div class="search__wrapper">
-                <input
-                type="text" 
-                placeholder="Найти книгу или автора...." 
-                class="search__input"
-                value="${this.state.searchQuery ? this.state.searchQuery : ''}"
-                />
-                <img src="/static/search.svg" alt="Иконка поиска" />
-            </div>
-            <button aria-label="Искать"><img src="/static/search-white.svg" alt="Иконка поиска"/></button>
-        `;
-        this.el.querySelector('button').addEventListener('click', this.search.bind(this));
-        this.el.querySelector('button').addEventListener('keydown', (event) => {
-            if (event.code === 'Enter')
-                this.search()
-            }
-        );
+            <h1>Найдено книг - ${this.parentState.list.length}</h1>
+        `   
         return this.el;
     }
 }
